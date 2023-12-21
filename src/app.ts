74bd730cf1abf9ea84416edfa0a7a270b32825eb
abhidesh10 <abhijitdeshmukh10@gmail.com>
2023-12-22 01:16:43 +0530

    code for handling non-existing endpoints

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/users", userRoutes);
 
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Sorry, the page you requested could not be found.",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
 });
