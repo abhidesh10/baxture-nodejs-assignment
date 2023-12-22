bca9825a68f68a7130e3cdc03eebe247ee29981c
abhidesh10 <abhijitdeshmukh10@gmail.com>
2023-12-22 13:06:20 +0530

    Handling error on server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import userRoutes from "./routes/userRoutes";
 import dotenv from "dotenv";
 
@@ -13,6 +13,14 @@ app.use("/api/users", userRoutes);
 app.use((req, res) => {
   res.status(404).json({
     error: "Sorry, the page you requested could not be found.",
+  });
+});
+
+// Handle 500 errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: "Sorry, something went wrong on our end. Please try again later.",
   });
 });
 
